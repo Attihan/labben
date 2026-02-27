fbbe0eeefcbc393863af6fa443517e7ef3d77785
Attihan <atrey5170@oslomet.no>
2026-02-27 18:41:12 +0100

    Added interactive page with embla carousel,

--- src/pages/Interactive.tsx
+++ src/pages/Interactive.tsx
@@ -1,9 +1,48 @@
+import { EmblaOptionsType } from 'embla-carousel'
+import Navbar from '../components/layout/Navbar'
+import Carousel from '../components/ui/Carousel'
+import '../styles/components/carousel.css'
+
+const OPTIONS: EmblaOptionsType = { align: 'start' }
+
+const SLIDES = [
+  {
+    id: 1,
+    title: 'Mining site',
+    url: '/interactive',
+    description: 'Mine the blocks and see the physics in action',
+    image: '/images/interactive/tooltip.jpg',
+  },
+  {
+    id: 2,
+    title: 'Scrolled animation site',
+    url: '/interactive',
+    description: 'A UI centered site focusing on animations.',
+    image: '/images/interactive/physics.jpg',
+  },
+  {
+    id: 3,
+    title: 'Ball physics site',
+    url: '/interactive',
+    description: 'A ball you can interact with, throw and bounce.',
+    image: '/images/interactive/materials.jpg',
+  },
+]
+
 function Interactive() {
-return (
-    <p>
-        test
-    </p>
-)
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <h2>The interactive area</h2>
+        <p>
+            Here you will be able to find interactive experiments. 
+        </p>
+      </div>
+
+      <Carousel slides={SLIDES} options={OPTIONS} />
+    </>
+  )
 }
 
 export default Interactive
